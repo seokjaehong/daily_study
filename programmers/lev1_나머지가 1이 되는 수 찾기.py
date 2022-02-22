b762b68a22130c1bbee5e0af89008bf4e429b5bb
Hong <hsj2334@gmail.com>
2022-02-22 13:00:45 +0900

    20220202

---
+++ programmers/lev1_나머지가 1이 되는 수 찾기.py
@@ -0,0 +1,11 @@
+def solution(n):
+    answer = 0
+    for x in range(1, n):
+        if n % x == 1:
+            answer = x
+            break
+    return answer
+
+
+if __name__ == '__main__':
+    print(solution(10))
