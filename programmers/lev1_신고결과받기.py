cb07dee5c72d1595376b3afbae87ca0be5e5f72c
Hong <hsj2334@gmail.com>
2022-06-28 15:54:10 +0900

    신고결과받기'

--- programmers/lev1_신고결과받기.py
+++ programmers/lev1_신고결과받기.py
@@ -1,40 +1,24 @@
 def solution(id_list, report, k):
-    report_dict = {x: 0 for x in id_list}
-    user_dict = {x: list() for x in id_list}
-    ck_list = []
-    for r in report:
-        kk, v = r.split(" ")
-        if r in ck_list:
-            report_dict[v] = 1
-            user_dict[kk] = [v]
-        else:
-            report_dict[v] += 1
-            user_dict[kk] += [v]
-        ck_list.append(r)
+    r_dict = {x: [0, False] for x in id_list}
+    for r in list(set(report)):
+        id, name = r.split(" ")
+        r_dict[name][0] += 1
+        if r_dict[name][0] >= k:
+            r_dict[name][1] = True
 
-    stop_id_dict = dict(filter(lambda elem: elem[1] >= k, report_dict.items()))
+    result_dict={x:0 for x in id_list}
+    for rp in list(set(report)):
+        id, name = rp.split(" ")
+        if r_dict[name][1]:
+            result_dict[id]+=1
+    return list(result_dict.values())
 
-    result = {x: 0 for x in id_list}
-    for u_k, u_v in user_dict.items():
-        for s_k, s_v in stop_id_dict.items():
-            if s_k in u_v:
-                result[u_k] += 1
-    answer = list(result.values())
-    return answer
 
-    # print(stop_id_list)
-    # result = {x: list() for x in id_list}
-    # for r in report:
-    #     a, b = r.split(" ")
-    #     if b in stop_id_list:
-    #         result[a] += [b]
-    # answer = [len(v) for k, v in result.items()]
-    # answer=[]
-    # return answer
 
 
 if __name__ == '__main__':
-    a = [["muzi", "frodo", "apeach", "neo"], ["muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi"], 2]
+    a = [["muzi", "frodo", "apeach", "neo"],
+         ["muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi"], 2]
     b = [["con", "ryan"], ["ryan con", "ryan con", "ryan con", "ryan con"], 3]
     c = [["con", "ryan", "frodo"], ["ryan frodo", "ryan con", "ryan con", "ryan con"], 1]
 
@@ -44,4 +28,5 @@ if __name__ == '__main__':
         (c, [0, 2, 0])
     ]
     for x, y in d:
+        # solution(x[0], x[1], x[2])
         print(y == solution(x[0], x[1], x[2]))
